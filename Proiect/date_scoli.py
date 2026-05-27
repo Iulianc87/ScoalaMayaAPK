b777ff27571c0159ef35817dc60722e20ebda548
Iulianc87 <cojocariu.iulian08@gmail.com>
2026-05-27 15:25:55 +0300

    Add files via upload

---
+++ Proiect/date_scoli.py
@@ -0,0 +1,34 @@
+# Fișierul date_scoli.py
+
+DATE_LOGICE = {
+    "cuvinte": {
+        "animale": ["pisică", "câine", "iepure", "pasăre", "vacă", "oaie", "cal", "pește", "rață", "găină", "urs", "lup", "vulpe", "porc", "capră", "măgar", "căprioară", "veveriță", "șoarece", "broască"],
+        "obiecte": ["creion", "carte", "masă", "telefon", "ghiozdan", "scaun", "penar", "lampă", "pernă", "jucărie", "caiet", "radiere", "riglă", "tablă", "dulap", "geam", "ușă", "pat", "ceas", "oglindă"]
+    },
+    "propozitii": {
+        "oameni": [
+            "Mama citește o carte.", "Maya scrie în caiet.", "Bunicul lucrează în grădină.", "Fratele gătește micul dejun.", "Învățătoarea povestește o legendă.",
+            "Sora desenează un soare.", "Colegul aleargă în parc.", "Tata repară mașina.", "Bunica coace plăcinte.", "Prietena cântă veselă.",
+            "Copilul învață alfabetul.", "Vecinul udă florile.", "Doctorul ajută bolnavii.", "Pictorul colorează tabloul.", "Sportivul câștigă cursa.",
+            "Muzicianul cântă la vioară.", "Grădinarul plantează copaci.", "Bucătarul prepară supa.", "Elevul ascultă lecția.", "Bunica tricotează șosete."
+        ],
+        "natura": [
+            "Pisica toarce pe pat.", "Cățelul latră la poartă.", "Iepurașul ronțăie morcovul.", "Pasărea zboară sus pe cer.", "Calul galopează pe câmp.",
+            "Soarele strălucește puternic.", "Luna apare noaptea.", "Stelele sclipesc pe cer.", "Ploaia udă pământul.", "Zăpada acoperă grădina.",
+            "Vântul suflă printre frunze.", "Râul curge spre mare.", "Muntele are vârf înalt.", "Florile înfloresc primăvara.", "Copacul este verde vara.",
+            "Albina adună miere.", "Fluturele zboară pe flori.", "Norii sunt albi și pufoși.", "Furtuna se apropie repede.", "Iarba este crudă și moale."
+        ]
+    },
+    "texte_dictare": [
+        "Mama vine de la serviciu mergând prin piață ca să cumpere pâine și mere.",
+        "Maya se joacă în curtea bunicilor cu un cățeluș mic și foarte jucăuș.",
+        "Afară plouă mărunt, iar eu stau în casă și citesc o poveste frumoasă.",
+        "Bunicul lucrează în grădina din spatele casei, unde a plantat mulți pomi.",
+        "Fratele meu gătește micul dejun în timp ce eu îmi pregătesc ghiozdanul pentru școală.",
+        "Toamna a sosit cu frunze aurii și vânt rece care suflă peste oraș.",
+        "La școală am învățat astăzi o lecție interesantă despre animalele din pădure.",
+        "Pisica stă ghemuită pe fotoliu și privește pe fereastră la păsările din pom.",
+        "Bunica a pregătit plăcinte delicioase cu mere și scorțișoară pentru toată familia.",
+        "Soarele a apus după dealuri, iar stelele au început să sclipească pe cerul negru."
+    ]
+}
